1789f34321e373c6f90196325a5c95c954b8339b
m-zz <marcusyzhu@gmail.com>
2021-03-15 11:42:21 -0700

    part 2 complete

--- app.py
+++ app.py
@@ -1,1 +1,42 @@
 """Flask app for Cupcakes"""
+from flask import Flask, render_template, redirect, request, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "secret"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+toolbar = DebugToolbarExtension(app)
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+
+connect_db(app)
+db.create_all()
+
+@app.route("/api/cupcakes")
+def get_all_cupcakes():
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialized() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+@app.route("/api/cupcakes/<int:cupcake_id>")
+def get_cupcake(cupcake_id):
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    return jsonify(cupcake=cupcake.serialized())
+
+@app.route("/api/cupcakes", methods = ["POST"])
+def create_cupcake():
+
+    r = request.json
+
+    new_cupcake = Cupcake(flavor=r["flavor"], size=r["size"], rating=int(r["rating"]), image=r["image"] or None)
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    return (jsonify(cupcake=new_cupcake.serialized()), 201)
--- models.py
+++ models.py
@@ -1,1 +1,35 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
+
+
+class Cupcake(db.Model):
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    image = db.Column(db.Text, nullable=False, default="https://thestayathomechef.com/wp-content/uploads/2017/12/Most-Amazing-Chocolate-Cupcakes-1-small.jpg")
+
+    def serialized(self):
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image": self.image 
+        }
