6b62fe8bf84108b4282ce7b5124377d83cd68285
johnathanraiss <johnathan.raiss@gmail.com>
2021-03-15 17:07:18 -0400

    part 4 finished

--- app.py
+++ app.py
@@ -17,26 +17,61 @@ app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 connect_db(app)
 db.create_all()
 
+
 @app.route("/api/cupcakes")
 def get_all_cupcakes():
+    """show all cupcakes"""
     cupcakes = Cupcake.query.all()
     serialized = [c.serialized() for c in cupcakes]
 
     return jsonify(cupcakes=serialized)
 
+
 @app.route("/api/cupcakes/<int:cupcake_id>")
 def get_cupcake(cupcake_id):
+    """get data for individual cupcake"""
     cupcake = Cupcake.query.get_or_404(cupcake_id)
 
     return jsonify(cupcake=cupcake.serialized())
 
-@app.route("/api/cupcakes", methods = ["POST"])
+
+@app.route("/api/cupcakes", methods=["POST"])
 def create_cupcake():
-
+    """creates cupcake with all properties"""
     r = request.json
-
+    print(r)
     new_cupcake = Cupcake(flavor=r["flavor"], size=r["size"], rating=int(r["rating"]), image=r["image"] or None)
     db.session.add(new_cupcake)
     db.session.commit()
 
     return (jsonify(cupcake=new_cupcake.serialized()), 201)
+
+
+@app.route("/api/cupcakes/<int:id>", methods=["PATCH"])
+def update_cupcake(id):
+    """update cupcake data and return updated data"""
+
+    r = request.json
+
+    cupcake = Cupcake.query.get_or_404(id)
+
+    cupcake.flavor = r["flavor"] 
+    cupcake.size = r["size"] 
+    cupcake.rating = int(r["rating"]) 
+    cupcake.image = r["image"] or "https://thestayathomechef.com/wp-content/uploads/2017/12/Most-Amazing-Chocolate-Cupcakes-1-small.jpg"
+
+    db.session.commit()
+
+    return jsonify(cupcake=cupcake.serialized())
+
+
+@app.route("/api/cupcakes/<int:id>", methods=["DELETE"])
+def delete_cupcake(id):
+    """ Delete cupcake and show message upon completion"""
+    
+    cupcake = Cupcake.query.get_or_404(id)
+
+    db.session.delete(cupcake)
+    db.session.commit()
+
+    return jsonify(message="Cupcake gone")
